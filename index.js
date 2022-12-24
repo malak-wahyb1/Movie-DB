c618b4f46e919c58d9a6eec218593719b125bb7e
malak-wahyb <malakwahyb537@gmail.com>
2022-12-24 14:36:23 +0200

    Step 3 - Create an express simple API

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+var date= new Date()
+var hour=date.getHours()
+var min=date.getMinutes()
+var sec=date.getSeconds()
 app.get('/', (req, res) =>{
     res.type('text/plain');
     res.send('ok');
@@ -9,9 +13,19 @@ app.get('/about', (req, res) =>{
     res.type('text/plain');
     res.send('Hello World about!');
 });
-app.use((req, res) =>{
-    res.type('text/plain');
-    res.statusCode(404);
-    res.send('Not Found');
-})
+// app.use((req, res) =>{
+//     res.type('text/plain');
+//     res.status(404);
+//     res.send('Not Found');
+// })
 app.listen(port, () =>{console.log(`listening on port ${port} clt c to get out`);});
+//url /test
+app.get('/test', (req, res) =>{
+    res.type('text/plain');
+    res.status(200)
+    res.send({status:200,message:'ok'});
+})
+app.get('/time', (req, res) =>{
+    res.type('text/plain');
+    res.send({status:200,message:`${hour}:${min}:${sec}`})
+});
